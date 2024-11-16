2669a769e84d79aaf61b1e12a4e02a8fabb7d2cd
taga3s <bepozurapu274@gmail.com>
2024-11-16 09:27:01 +0900

    done

---
+++ seed.ts
@@ -0,0 +1,49 @@
+import fs from "fs";
+import csv from "csv-parser";
+import { Index } from "@upstash/vector"
+
+interface Row {
+	text: string;
+}
+
+const index = new Index({
+  url: "",
+  token: "",
+})
+
+async function parseCSV(filePath: string): Promise<Row[]> {
+	return new Promise((resolve, reject) => {
+		const rows: Row[] = [];
+
+		fs.createReadStream(filePath)
+			.pipe(csv({ separator: "," }))
+			.on("data", (row) => {
+        rows.push(row);
+      })
+      .on("error", (err) => {
+        reject(err);
+      })
+      .on("end", () => {
+        resolve(rows);
+      });
+	});
+}
+
+const STEP = 30;
+const seed = async () => {
+  const data = await parseCSV("./training_data.csv");
+
+  for (let i = 0; i < data.length; i += STEP) {
+    const chunk = data.slice(i, i + STEP);
+
+    const formatted = chunk.map((row, batchIndex) => ({
+      data: row.text,
+      id: i + batchIndex,
+      metadata: { text: row.text },
+    }))
+
+    await index.upsert(formatted);
+  }
+}
+
+seed();
